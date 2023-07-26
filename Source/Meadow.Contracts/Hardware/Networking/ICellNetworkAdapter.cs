f81fe59ea44b1de8b76d6f7d9bcfce541e0d8d45
Chris Tacke <ctacke@opennetcf.com>
2023-07-26 12:03:01 -0500

    xml docs

--- Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
+++ Source/Meadow.Contracts/Hardware/Networking/ICellNetworkAdapter.cs
@@ -1,6 +1,8 @@
-﻿namespace Meadow.Hardware
+﻿namespace Meadow.Hardware;
+
+/// <summary>
+/// Provides an abstraction for a cellular INetworkAdapter
+/// </summary>
+public interface ICellNetworkAdapter : INetworkAdapter
 {
-    public interface ICellNetworkAdapter : INetworkAdapter
-    {
-    }
 }
