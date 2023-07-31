9eef787d0de635ada22234b970fbd63df1955765
halyssonJr <halysson1007@gmail.com>
2023-07-31 16:24:53 -0300

    fix struct element index to status

--- Source/Meadow.Contracts/Hardware/Networking/CellNetwork.cs
+++ Source/Meadow.Contracts/Hardware/Networking/CellNetwork.cs
@@ -2,7 +2,7 @@
 
 public record CellNetwork
 {
-    public int Index { get; set; }
+    public string Status { get; set; }
     public string Name { get; set; }
     public string Operator { get; set; }
     public string Code { get; set; }
