6e53e22348e08d6fb5ae9bcb0e7eeea0a3e5de3e
Chris Tacke <ctacke@opennetcf.com>
2024-06-17 10:31:39 -0500

    move classes to their own files

---
+++ Source/Meadow.Contracts/Hardware/Contracts/Connectors/SensorCollection.cs
@@ -0,0 +1,22 @@
+﻿using Meadow.Peripherals.Sensors;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Meadow.Hardware;
+
+/// <summary>
+/// Represents a Collection of ISensors
+/// </summary>
+public class SensorCollection : IEnumerable<ISensor>
+{
+    /// <inheritdoc/>
+    public IEnumerator<ISensor> GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        throw new System.NotImplementedException();
+    }
+}
