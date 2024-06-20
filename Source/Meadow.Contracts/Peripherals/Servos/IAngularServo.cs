e58b4351be21f69c5e997a59d5e4772c8535400e
Chris Tacke <ctacke@opennetcf.com>
2024-06-20 11:48:11 -0500

    pull servo interfaces into Contracts

---
+++ Source/Meadow.Contracts/Peripherals/Servos/IAngularServo.cs
@@ -0,0 +1,22 @@
+﻿using Meadow.Units;
+using System.Threading.Tasks;
+
+namespace Meadow.Peripherals.Servos;
+
+/// <summary>
+/// Represents an angular servo interface, extending the basic servo interface with additional properties and methods for controlling angular position.
+/// </summary>
+public interface IAngularServo : IServo
+{
+    /// <summary>
+    /// Rotates the servo to the specified angle.
+    /// </summary>
+    /// <param name="angle">The target angle to rotate to.</param>
+    /// <returns>A task that represents the asynchronous rotate operation.</returns>
+    Task RotateTo(Angle angle);
+
+    /// <summary>
+    /// Gets the current angle of the servo.
+    /// </summary>
+    Angle? Angle { get; }
+}
