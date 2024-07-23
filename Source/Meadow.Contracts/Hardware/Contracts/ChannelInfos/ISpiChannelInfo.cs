cc264024ca1f239e93e80b520e68dbb575adfd24
Chris Tacke <ctacke@opennetcf.com>
2024-07-23 12:12:09 -0500

    Add bus number to SPI

--- Source/Meadow.Contracts/Hardware/Contracts/ChannelInfos/ISpiChannelInfo.cs
+++ Source/Meadow.Contracts/Hardware/Contracts/ChannelInfos/ISpiChannelInfo.cs
@@ -9,5 +9,9 @@
         /// Gets the line types supported by the SPI channel.
         /// </summary>
         SpiLineType LineTypes { get; }
+        /// <summary>
+        /// Gets the system bus number for the channel
+        /// </summary>
+        int BusNumber { get; }
     }
 }
