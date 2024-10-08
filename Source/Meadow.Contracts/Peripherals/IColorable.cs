1e3168454df662e5b8363c5900afb7bcc8a002c4
Chris Tacke <ctacke@opennetcf.com>
2024-10-08 13:16:34 -0500

    add icolorable

---
+++ Source/Meadow.Contracts/Peripherals/IColorable.cs
@@ -0,0 +1,18 @@
+﻿namespace Meadow.Hardware;
+
+/// <summary>
+/// Represents an item that can be colored
+/// </summary>
+public interface IColorable
+{
+    /// <summary>
+    /// Sets the color of the target
+    /// </summary>
+    /// <param name="color">The color to set</param>
+    void SetColor(Color color);
+
+    /// <summary>
+    /// Gets the color of the target
+    /// </summary>
+    Color GetColor();
+}
